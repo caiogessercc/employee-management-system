30cb5c8646638628ff803c13133b7271f00250b8
Caio Gesser Custodio <caiogesserc3@gmail.com>
2026-04-11 17:46:55 -0300

    feat(main): implementa serviço de funcionários e inicializa lista de empregados

--- Main.java
+++ Main.java
@@ -1,6 +1,16 @@
+import application.EmployeeService;
+import domain.Employee;
+import infrastructure.bootstrap.DataProvider;
+
+import java.util.List;
+
 public class Main {
 
   public static void main(String[] args) {
+
+    EmployeeService service = new EmployeeService();
+
+    List<Employee> employees = DataProvider.createEmployees();
 
   }
 }
