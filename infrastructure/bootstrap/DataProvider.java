df9500e897a5f4a31ef7bf4f846cb1f36ad2dd26
Caio Gesser Custodio <caiogesserc3@gmail.com>
2026-04-11 16:40:34 -0300

    feat(infrastructure): inicializa lista de funcionários

--- infrastructure/bootstrap/DataProvider.java
+++ infrastructure/bootstrap/DataProvider.java
@@ -3,11 +3,16 @@ package infrastructure.bootstrap;
 import domain.Employee;
 
 import java.util.List;
+import java.util.ArrayList;
 
 public class DataProvider {
   private DataProvider() {}
 
   public static List<Employee> createEmployees() {
-    return null;
+    List<Employee> employees = new ArrayList<>();
+
+    // TODO: Adicionar funcionários ao DataProvider
+
+    return employees;
   }
 }
