7690211b8bd7481797274ffdfab215fd582c7849
Caio Gesser Custodio <caiogesserc3@gmail.com>
2026-04-11 00:49:27 -0300

    feat(application): adiciona método removeByName  para remoção de funcionários por nome

--- application/EmployeeService.java
+++ application/EmployeeService.java
@@ -1,5 +1,12 @@
 package application;
 
+import domain.Employee;
+
+import java.util.List;
+
 public class EmployeeService {
-  
+
+  public void removeByName(List<Employee> employees, String name) {
+    employees.removeIf(e -> e.getName().equalsIgnoreCase(name));
+  }
 }
