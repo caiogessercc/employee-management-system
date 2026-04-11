a146ac5f55555e5fecb4e355eeca4894b0e6c27f
Caio Gesser Custodio <caiogesserc3@gmail.com>
2026-04-11 00:51:26 -0300

    feat(application): adiciona método applySalaryIncrease para aumento de salário dos funcionários

--- application/EmployeeService.java
+++ application/EmployeeService.java
@@ -2,11 +2,16 @@ package application;
 
 import domain.Employee;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 public class EmployeeService {
 
   public void removeByName(List<Employee> employees, String name) {
     employees.removeIf(e -> e.getName().equalsIgnoreCase(name));
+  }
+
+  public void applySalaryIncrease(List<Employee> employees, BigDecimal percentage) {
+    employees.forEach(e -> e.applySalaryIncrease(percentage));
   }
 }
