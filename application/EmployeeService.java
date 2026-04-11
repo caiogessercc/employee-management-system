6b722e53a4aedacd07223ebb79ffa26b2ecf5325
Caio Gesser Custodio <caiogesserc3@gmail.com>
2026-04-11 16:07:58 -0300

    refactor(application): remove importação não utilizada de HashMap na classe EmployeeService

--- application/EmployeeService.java
+++ application/EmployeeService.java
@@ -7,7 +7,6 @@ import java.math.RoundingMode;
 import java.time.Month;
 import java.util.List;
 import java.util.Map;
-import java.util.HashMap;
 import java.util.Comparator;
 import java.util.stream.Collectors;
 
