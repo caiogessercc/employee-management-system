dc76e9cd37009b3a464546f8b381377ef60be631
Caio Gesser Custodio <caiogesserc3@gmail.com>
2026-04-11 17:35:28 -0300

    feat(infrastructure): adiciona formatador de data no Formatter

--- infrastructure/formatting/Formatter.java
+++ infrastructure/formatting/Formatter.java
@@ -1,5 +1,9 @@
 package infrastructure.formatting;
 
+import java.time.format.DateTimeFormatter;
+
 public class Formatter {
   private Formatter() {}
+
+  private static final DateTimeFormatter DATE_FORMATTER = DateTimeFormatter.ofPattern("dd/MM/yyyy");
 }
