ece8ad6f272221da67604323b078c86980119cd6
Caio Gesser Custodio <caiogesserc3@gmail.com>
2026-04-11 16:37:38 -0300

    feat(infrastructure): adiciona método para criar lista de funcionários no DataProvider

--- infrastructure/bootstrap/DataProvider.java
+++ infrastructure/bootstrap/DataProvider.java
@@ -1,5 +1,13 @@
 package infrastructure.bootstrap;
 
+import domain.Employee;
+
+import java.util.List;
+
 public class DataProvider {
   private DataProvider() {}
+
+  public static List<Employee> createEmployees() {
+    return null;
+  }
 }
