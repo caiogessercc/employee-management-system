02c7f1d5d702eed013f6bfbacf1624d3940d5221
Caio Gesser Custodio <caiogesserc3@gmail.com>
2026-04-11 17:36:41 -0300

    feat(infrastructure): adiciona formatador de moeda no Formatter

--- infrastructure/formatting/Formatter.java
+++ infrastructure/formatting/Formatter.java
@@ -1,9 +1,12 @@
 package infrastructure.formatting;
 
 import java.time.format.DateTimeFormatter;
+import java.text.NumberFormat;
+import java.util.Locale;
 
 public class Formatter {
   private Formatter() {}
 
   private static final DateTimeFormatter DATE_FORMATTER = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+  private static final NumberFormat MONEY_FORMATTER = NumberFormat.getNumberInstance(Locale.forLanguageTag("pt-BR"));
 }
